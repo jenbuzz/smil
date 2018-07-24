e5e3483dd1bb26a6373f1f3c0252413e6c9dc8bc
dan-lyn <daniel_lynge@hotmail.com>
2018-07-24 09:14:45 +0200

    using new search in emoji too

--- emoji.js
+++ emoji.js
@@ -1,22 +1,16 @@
 'use strict';
 
 const emojilib = require('emojilib');
-const fuzzysearch = require('fuzzysearch');
+const search = require('./search');
 
 module.exports = input => {
     if (emojilib.lib[input] && emojilib.lib[input].char) {
         return emojilib.lib[input].char;
     }
 
-    const searchResults = [];
-    Object.keys(emojilib.lib).forEach(name => {
-        const result = fuzzysearch(input, name);
-        if (result) {
-            searchResults.push(name);
-        }
-    });
+    const searchResults = search(input);
 
     if (searchResults.length > 0) {
-        return emojilib.lib[searchResults[0]].char;
+        return searchResults[0];
     }
 };
